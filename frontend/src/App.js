7b6c93319d7d223ff480bdff580aca6dafb87b40
Doudou210 <siakakoudou.doumbia@yahoo.com>
2024-07-15 19:28:47 +0200

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Chargement from './pages/Chargement';
 import PageConnexion from './pages/PageConnexion';
 import Connexion from './pages/Connexion';
 import Inscription from './pages/Inscription';
@@ -14,12 +13,11 @@ function App() {
   return (
     <div >
       <Routes>
-        <Route path='/' Component={Chargement}/>
+        <Route path='/' Component={Accueil}/>
         <Route path='/PageConnexion' Component={PageConnexion}/>
         <Route path='/connexion' Component={Connexion}/>
         <Route path='/inscription' Component={Inscription}/>
         <Route path='/mdp' Component={Mdp}/>
-        <Route path='/accueil' Component={Accueil}/>
         <Route path='/accueil/paiement' Component={Paiement}/>
         <Route path='/accueil/details' Component={Details}/>
         <Route path='/return' Component={Return}/>
