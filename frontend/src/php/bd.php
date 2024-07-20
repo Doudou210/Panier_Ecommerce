42a06d1e6fa12022c543a4ba8ae21c07b8fa191c
Doudou210 <siakakoudou.doumbia@yahoo.com>
2024-07-20 16:19:42 +0200

    move folders

--- frontend/src/php/bd.php
+++
@@ -1,8 +0,0 @@
-<?php
-    try {
-        $bd = new PDO ("mysql:host=hostname; dbname=resto", "root", "");
-        $bd -> setAttribute(PDo::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        die("Erruer de connexion:" .$e -> getMessage());
-    }
-?>
---
+++ src/connexion/Register.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+function RegisterPage({ handleRegister }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleRegisterFormSubmit = async (event) => {
+    event.preventDefault();
+    // handleRegister(username, password);
+    try {
+        const response = await fetch('http://localhost:7000/register', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ username, password }),
+        });
+        const data = await response.json();
+        if (response.ok) {
+          alert(data.message);
+        } else {
+          alert('Registration failed');
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      <form onSubmit={handleRegisterFormSubmit}>
+        <input type="text" placeholder="Username" value={username} onChange={handleUsernameChange} />
+        <input type="password" placeholder="Password" value={password} onChange={handlePasswordChange} />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default RegisterPage;
